489fe3d6180d7824ee35df35e0a229d5cae53bf5
Marcus Larsson <marcuslarsson_92@hotmail.com>
2018-12-26 13:15:45 +0100

    removed unnessasary code from chain

--- include/chain.hpp
+++ include/chain.hpp
@@ -10,18 +10,17 @@
 namespace fn {
 namespace detail {
 
-template <std::size_t Index, typename Tuple, typename... Args>
+template <std::size_t I, typename Tuple, typename... Args>
 constexpr decltype(auto) recurse_or_call_index(Tuple&& tuple, Args&&... args);
 
-template <std::size_t Index, typename Tuple, typename... Args>
+template <std::size_t I, typename Tuple, typename... Args>
 constexpr decltype(auto) call_index(Tuple&& tuple, Args&&... args) {
-    using fn_sig = decltype(std::get<Index>(std::forward<Tuple>(tuple)));
-
+    using fn_sig = decltype(std::get<I>(std::forward<Tuple>(tuple)));
     constexpr bool invocable =
         std::is_invocable_v<fn_sig, decltype(std::forward<Args>(args))...>;
 
     if constexpr (invocable) {
-        return std::invoke(std::get<Index>(std::forward<Tuple>(tuple)),
+        return std::invoke(std::get<I>(std::forward<Tuple>(tuple)),
                            std::forward<Args>(args)...);
     } else {
         constexpr bool applyable =
@@ -29,49 +28,47 @@ constexpr decltype(auto) call_index(Tuple&& tuple, Args&&... args) {
 
         static_assert(applyable, "fn::chain functions does not match");
 
-        return std::apply(std::get<Index>(std::forward<Tuple>(tuple)),
+        return std::apply(std::get<I>(std::forward<Tuple>(tuple)),
                           std::forward<Args>(args)...);
     }
 }
 
-template <std::size_t Index, typename Tuple, typename... Args>
+template <std::size_t I, typename Tuple, typename... Args>
 constexpr decltype(auto) recurse_index(Tuple&& tuple, Args&&... args) {
-    using fn_sig = decltype(std::get<Index>(std::forward<Tuple>(tuple)));
-
+    using fn_sig = decltype(std::get<I>(std::forward<Tuple>(tuple)));
     constexpr bool invocable =
-        std::is_invocable_v<fn_sig, decltype(recurse_or_call_index<Index - 1>(
+        std::is_invocable_v<fn_sig, decltype(recurse_or_call_index<I - 1>(
                                         std::forward<Tuple>(tuple),
                                         std::forward<Args>(args)...))>;
 
     if constexpr (invocable) {
         return std::invoke(
-            std::get<Index>(std::forward<Tuple>(tuple)),
-            recurse_or_call_index<Index - 1>(std::forward<Tuple>(tuple),
-                                             std::forward<Args>(args)...));
+            std::get<I>(std::forward<Tuple>(tuple)),
+            recurse_or_call_index<I - 1>(std::forward<Tuple>(tuple),
+                                         std::forward<Args>(args)...));
     } else {
         constexpr bool applyable =
-            fn::is_applyable_v<fn_sig,
-                               decltype(recurse_or_call_index<Index - 1>(
-                                   std::forward<Tuple>(tuple),
-                                   std::forward<Args>(args)...))>;
+            fn::is_applyable_v<fn_sig, decltype(recurse_or_call_index<I - 1>(
+                                           std::forward<Tuple>(tuple),
+                                           std::forward<Args>(args)...))>;
 
         static_assert(applyable, "fn::chain functions does not match");
 
         return std::apply(
-            std::get<Index>(std::forward<Tuple>(tuple)),
-            recurse_or_call_index<Index - 1>(std::forward<Tuple>(tuple),
-                                             std::forward<Args>(args)...));
+            std::get<I>(std::forward<Tuple>(tuple)),
+            recurse_or_call_index<I - 1>(std::forward<Tuple>(tuple),
+                                         std::forward<Args>(args)...));
     }
 }
 
-template <std::size_t Index, typename Tuple, typename... Args>
+template <std::size_t I, typename Tuple, typename... Args>
 constexpr decltype(auto) recurse_or_call_index(Tuple&& tuple, Args&&... args) {
-    if constexpr (Index == 0) {
-        return call_index<Index>(std::forward<Tuple>(tuple),
-                                 std::forward<Args>(args)...);
+    if constexpr (I == 0) {
+        return call_index<I>(std::forward<Tuple>(tuple),
+                             std::forward<Args>(args)...);
     } else {
-        return recurse_index<Index>(std::forward<Tuple>(tuple),
-                                    std::forward<Args>(args)...);
+        return recurse_index<I>(std::forward<Tuple>(tuple),
+                                std::forward<Args>(args)...);
     }
 }
 
@@ -81,9 +78,9 @@ template <typename... Fs>
 constexpr auto chain(Fs&&... fs) {
     return [fs = std::make_tuple(std::forward<Fs>(fs)...)](
                auto&&... args) -> decltype(auto) {
-        return detail::recurse_or_call_index<
-            std::tuple_size_v<std::remove_reference_t<decltype(fs)>> - 1>(
-            std::move(fs), std::forward<decltype(args)>(args)...);
+        constexpr std::size_t tuple_size = std::tuple_size_v<decltype(fs)>;
+        return detail::recurse_or_call_index<tuple_size - 1>(
+            fs, std::forward<decltype(args)>(args)...);
     };
 }
 
